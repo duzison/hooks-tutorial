4e120a7a1ffdb761ea992af0b4813cff1e48b841
sojin.son <sojin.son96@gmail.com>
2022-12-10 15:18:36 +0900

    8.2 userEffect

--- src/Info.js
+++ src/Info.js
@@ -1,8 +1,41 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
+
+    // useEffect(()=> {
+    //     console.log('렌더링이 완료되었습니다!');
+    //     console.log({
+    //         name,
+    //         nickname
+    //     });
+    // });
+
+    // useEffect(()=> {
+    //     console.log('마운트될 때만 실행됩니다.');
+    // },[]);
+
+    // useEffect(()=> {
+    //     console.log(name);
+    // },[name]);
+
+    // useEffect(()=> {
+    //     console.log('effect');
+    //     console.log(name);
+    //     return () =>{
+    //         console.log('cleanup');
+    //         console.log(name);
+    //     }
+    // },[name]);
+
+     useEffect(()=> {
+        console.log('effect');
+        return () =>{
+            console.log('unmount');
+        };
+    },[]);
+
 
     const onChangeName = e => {
         setName(e.target.value);
