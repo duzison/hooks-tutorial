d096105facfa239d13bceafdf2c5b4ee4702f029
sojin.son <sojin.son96@gmail.com>
2022-12-10 15:47:10 +0900

    8.5 userCallback : 불필요하게 반복되는 함수 생성에 사용됨

--- src/Average.js
+++ src/Average.js
@@ -1,4 +1,4 @@
-import {useState, useMemo} from 'react';
+import {useState, useMemo, useCallback} from 'react';
 
 const getAverage = numbers => {
     console.log('평균값 계산 중..');
@@ -11,14 +11,24 @@ const Average = () =>{
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
 
-    const onChange = e => {
-        setNumber(e.target.value);
-    };
-    const onInsert = e => {
+    // const onChange = e => {
+    //     setNumber(e.target.value);
+    // };
+    // const onInsert = e => {
+    //     const nextList = list.concat(parseInt(number));
+    //     setList(nextList);
+    //     setNumber('');
+    // };
+
+    const onChange = useCallback(e => {
+        setNumber(e.target.value); 
+    }, []); // 컴포넌트가 처음 랜더링될 때만 함수 생성
+
+    const onInsert = useCallback(()=>{
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
-    };
+    }, [number, list]); // number 혹은 list가 바뀌었을 때만 함수 생성
 
     const avg = useMemo(() => getAverage(list), [list]);
 
